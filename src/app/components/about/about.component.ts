08b526a5cae170700f16e008d92fba1bb27e0bdd
Guilherme Pavezzi <gui.pavezzi07@gmail.com>
2024-10-12 19:28:54 -0300

    Arrumando texto

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -10,9 +10,9 @@ import { Component } from '@angular/core';
 export class AboutComponent {
 
 
-  about: string = 'Meu nome é Guilherme Fernandes Pavezi e sou um desenvolvedor de software full stack, tenho um profundo amor por tecnologia e pela arte de'+ 
-  'construir soluções digitais. Com uma sólida formação em desenvolvimento de software em C# e uma paixão ' +
-    'constante por aprender novas tecnologias, estou sempre em busca de oportunidades para aplicar minhas'+
-      'habilidades em projetos inovadores. Minha experiência abrange em criações e melhorias de APIs e microsserviços' + 
+  about: string = 'Meu nome é Guilherme Fernandes Pavezi e sou um desenvolvedor de software full stack, tenho um profundo amor por tecnologia e pela arte de '+ 
+  'construir soluções digitais. Com uma sólida formação em desenvolvimento de software em C# e Angular com uma paixão ' +
+    'constante por aprender novas tecnologias, estou sempre em busca de oportunidades para aplicar minhas '+
+      'habilidades em projetos inovadores. Minha experiência abrange em criações e melhorias de APIs e microsserviços ' + 
       'e na criações de telas'
 }
