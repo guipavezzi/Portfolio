265164169bf1d2ec641ec4ba753713db50f699f0
guipavezzi <gui.pavezzi07@gmail.com>
2025-09-30 12:43:27 -0300

    removing passin project, adding a new project in construction and change icons

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, MatIcon],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
