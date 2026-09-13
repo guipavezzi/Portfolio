966e5e644310ee25dfdcac5c12018ba3b14b3021
guipavezzi <gui.pavezzi07@gmail.com>
2026-09-12 22:04:17 -0300

    feat: add intro and footer components with social links and email copying functionality

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { CdkMenuModule } from '@angular/cdk/menu';
+import { ClipboardModule, Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CdkMenuModule, ClipboardModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { addIconSelectors } from '@iconify/tailwind'
 import { TranslateModule } from '@ngx-translate/core';
-
+import { CdkMenuModule } from '@angular/cdk/menu';
+import { ClipboardModule, Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-intro',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CdkMenuModule, ClipboardModule],
   providers: [],
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss'
 })
 export class IntroComponent {
+  email = 'gui.pavezzi07@gmail.com';
+
+  constructor(private clipboard: Clipboard) { }
+
+  copyEmail() {
+    this.clipboard.copy(this.email);
+  }
 }
